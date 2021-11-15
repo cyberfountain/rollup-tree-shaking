4557f08163716a22526b0e792308790b80c8edce
Lukasz Wisniewski <lwisniewski387@gmail.com>
2021-11-15 22:16:11 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rollup Tree Shaking
+
+Supporting materials for a Youtube lesson: [Youtube](https://www.youtube.com/watch?v=gSrVEn5TLPs&ab_channel=CyberFountain)
+
+![image](https://cyberfountain.ams3.digitaloceanspaces.com/images/rollup-tree-shaking.png)
---
+++ src/ts/app.ts
@@ -0,0 +1,18 @@
+import { func1, func2 } from "./Modules/Shake"
+import Unshakable from "./Modules/Unshakable"
+
+const un = new Unshakable();
+un.method1();
+
+func1();
+func2();
+
+(async () => {
+    const m = await import("./Modules/Dog")
+        .then(m => m.default)
+
+    const dog = new m()
+    dog.bark()
+})()
+
+console.log("ts")
